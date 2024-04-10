4ff084c0962928e528c7f8ec05e27cfce07c2b67
최성민 <sungmin2517@naver.com>
2024-04-10 23:32:54 +0900

    update

---
+++ choi/240410.py
@@ -0,0 +1,18 @@
+import sys
+from collections import Counter
+
+n = int(sys.stdin.readline())
+inp = list(map(int, sys.stdin.readline().split()))
+dict = {}
+stack=[]
+answer = [-1]*n
+
+dict = Counter(inp)
+
+for i in range(n):
+    while stack and dict[inp[stack[-1]]] < dict[inp[i]]:
+        idx = stack.pop()
+        answer[idx] = inp[i]
+    stack.append(i)
+
+print(*answer)
